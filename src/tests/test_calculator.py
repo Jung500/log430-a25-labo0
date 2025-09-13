d0815cc12c7950274067df25f74e33e4b1fa4a9e
Jung500 <jphilippeu@gmail.com>
2025-09-13 00:16:07 -0400

    Tests pour calculator.py

--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -6,8 +6,19 @@ Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 
 from calculator import Calculator
 
+my_calculator = Calculator()
 def test_app():
-    my_calculator = Calculator()
     assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
 # TODO: ajoutez les tests
+def test_addition():
+    assert my_calculator.addition(2, 3) == 5
+def test_subtraction():
+    assert my_calculator.subtraction(4, 2) == 2
+def test_multiplication():
+    assert my_calculator.multiplication(2, 2) == 4
+def test_division():
+    assert my_calculator.division(9, 3) == 3
+def test_error():
+    assert my_calculator.division(9, 3) == 2
+
